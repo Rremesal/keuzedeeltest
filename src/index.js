a4e722c3c7421b750899d77c7b5367c2f29f55e4
Raul Remesal van Merode <raul.remesal@gmail.com>
2023-10-13 14:56:47 +0200

    commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+function calculate(x, operator ,y) {
+
+    let result = 0;
+
+    switch(operator) {
+        case '+':
+            result = x + y;
+        break;
+        case '-':
+            result = x - y;
+        break;
+        case '/':
+            result = x / y;
+            break;
+        case '*':
+            result = x * y;
+    }
+    
+    return result
+}
---
+++ test.js
@@ -0,0 +1,7 @@
+
+function test() {
+    if (calculate(10, '+', 15) === 15) {
+        return true;
+    }
+    return false;
+}
